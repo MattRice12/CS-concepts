a7a0d89212bb45d6f2cc529ddaf8b411832ec546
Matt Rice <mattrice12@outlook.com>
2016-10-27 14:30:10 -0500

    merge sort implementation

--- sort-algorithms/mergesort.rb
+++ sort-algorithms/mergesort.rb
@@ -1,19 +1,27 @@
 # 1) Divide the unsorted list into n sublists, each containing 1 element (a list of 1 element is considered sorted).
 # 2) Repeatedly merge sublists to produce new sorted sublists until there is only 1 sublist remaining. This will be the sorted list.
 
-def mergesort(arr)
-  # arr_new = arr.partition { |i| (arr.index(i) + 1) <= (arr.length / 2)}
-  arr_new = recursive_partition(arr)
-  print arr_new
-end
-
-def recursive_partition(arr)
-  arr = arr.partition { |i| (arr.index(i) + 1) <= (arr.length / 2)}
-  arr = arr.map do |a|
-    a = recursive_partition(a) unless a.length <= 1
-    a
+def merge_sort(arr)
+  if arr.length <= 1
+    arr
+  else
+    mid = (arr.length / 2).floor
+    left = merge_sort(arr[0..mid - 1])
+    right = merge_sort(arr[mid..arr.length])
+    merge(left, right)
   end
-  arr
 end
 
-mergesort([1, 7, 10, 5, 3, 2, 6, 9, 11])
+def merge(left, right)
+  if left.empty?
+    right
+  elsif right.empty?
+    left
+  elsif left.first < right.first
+    [left.first] + merge(left[1..left.length], right)
+  else
+    [right.first] + merge(left, right[1..right.length])
+  end
+end
+
+print merge_sort([1, 7, 10, 5, 3, 2, 6, 9, 11])
