ada166d1731cda0fa1e80548fd3f10e5fb50d0dd
Matt Rice <mattrice12@outlook.com>
2016-10-29 16:17:01 -0500

    adding descriptions and info to sorting algorithms

--- sort-algorithms/quicksort.rb
+++ sort-algorithms/quicksort.rb
@@ -1,3 +1,13 @@
+# Complexity:
+  ## best:    O(n log n)
+  ## average: O(n log n)
+  ## worst:   O(n^2)
+
+# Algorithm:
+  ## Pick an element, called a pivot, from the array.
+  ## Partitioning: reorder the array so that all elements with values less than the pivot come before the pivot, while all elements with values greater than the pivot come after it (equal values can go either way). After this partitioning, the pivot is in its final position. This is called the partition operation.
+  ## Recursively apply the above steps to the sub-array of elements with smaller values and separately to the sub-array of elements with greater values.
+
 class Array
   def quicksort
     return [] if empty?
@@ -32,6 +42,9 @@ arr3 = [5, 4, 3, 2, 1]
 puts arr.quicksort.inspect
 
 puts arr2.quicksort.inspect
+
+puts arr3.quicksort.inspect
+
 
 # puts arr.quicksort_two.inspect
 # puts arr3.quicksort_two.inspect
--- sort-algorithms/selectionsort.rb
+++ sort-algorithms/selectionsort.rb
@@ -1,3 +1,17 @@
+# Complexity:
+  ## best:    O(n^2)
+  ## average: O(n^2)
+  ## worst:   O(n^2)
+
+# Algorithm:
+  ## 1) Find the smallest element in the unsorted sublist
+    ### Iterate through the sublist, comparing each element to find the smallest
+  ## 2) Exchange it with the leftmost unsorted element
+    ### swap the smallest element (min) with the first element in the sublist (i)
+  ## 3) Move the sublist boundaries one element to the right.
+    ### iteration = ((i + 1)..(size - 1))
+
+
 class Array
   def selection_sort
     (size - 1).times do |i|
